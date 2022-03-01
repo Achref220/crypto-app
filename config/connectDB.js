075fa94c4930c4765afa7a60fdc996e9b6e051c8
Achref <achref.najeh5@gmail.com>
2022-03-01 12:21:18 +0100

    upd

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://achref25:achref2522@cluster0.e9c0p.mongodb.net/crypto_web?retryWrites=true")
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log("the database is connected");
     } catch (err) {
         console.log(err);
